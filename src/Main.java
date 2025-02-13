47e7295c8fb91464b9b7b63759840c912fd04df9
kay9033 <kay903347@gmail.com>
2025-02-13 23:20:13 +0900

    ピラミッド

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    CPU cpu = new CPU("intel i9");
-    Memory memory = new Memory(16);
-
-    Computer computer = new Computer(cpu, memory);
-
-    computer.run();
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("入力してください: ");
+    String input = scanner.nextLine();
+    String[] inputArray = input.split("");
+    for (String str : inputArray) {
+      Integer num = Integer.parseInt(str);
+      for (int i = 1; i <= (inputArray.length - num); i++) {
+        System.out.printf(" ");
+      }
+      for (int i = 1; i < num; i++) {
+        System.out.printf(String.valueOf(i));
+      }
+      System.out.printf(str);
+      for (int i = 1; i < num; i++) {
+        System.out.printf(String.valueOf(num - i));
+      }
+      System.out.printf("%n");
+    }
+    scanner.close();
   }
 }
